19b237034b825fdf3f14da4dd36adf8aac48bdaf
Nick McGreivy <mcgreivy@princeton.edu>
2020-02-16 12:52:57 -0500

    Preparing to write internal functions in CoilSet.py

--- focusadd/coils/CoilSet.py
+++ focusadd/coils/CoilSet.py
@@ -33,52 +33,7 @@ class CoilSet:
 			self.fr = np.zeros((6,self.NC,self.NFR))
 		else:
 			raise Exception("No file or args_dict passed to initialize coil set. ")
-		#self.set_params((self.fc, self.fr))
-
-	def get_r_central(self):
-		return self.r_central
-
-	def set_params(self, params):
-		# UNPACK PARAMS
-		self.fc, self.fr = params
-		# COMPUTE THINGS
-		self.compute_r()
-		self.compute_x1y1z1()
-		self.compute_x2y2z2()
-		self.compute_x3y3z3()
-		self.compute_dsdz()
-		self.compute_frenet()
-		self.compute_torsion()
-		self.compute_curvature()
-		self.compute_integrated_torsion()
-		self.compute_integrated_curvature()
-
-	def get_params(self):
-		return (self.fc, self.fr)
-
-	def compute_r(self):
-		pass
-
-	def get_r(self):
-		return self.r
-
-	def get_params(self):
-		return self.fc, self.fr
-
-	def write(self, output_file):
-		""" Write coils in HDF5 output format"""
-		with tb.open_file(output_file, 'w') as f:
-			metadata = numpy.dtype([('NC', int), ('NS', int),('NF', int),('NFR',int),('ln',float),('lb',float),('NNR',int),('NBR',int),('rc',float),('NR',int)])
-			arr = numpy.array([(self.NC, self.NS, self.NF, self.NFR, self.ln, self.lb, self.NNR, self.NBR, self.rc, self.NR)],dtype=metadata)
-			f.create_table('/','metadata',metadata)
-			f.root.metadata.append(arr)
-			f.create_array('/','coilSeries',numpy.asarray(self.fc))
-			f.create_array('/','rotationSeries',numpy.asarray(self.fr))
-
-
-
-	def calc_length(self):
-		pass
+		self.set_params((self.fc, self.fr))
 
 	def compute_coil_fourierSeries(self,r_central):
 		x = r_central[:,0:-1,0] # NC x NS
@@ -95,18 +50,57 @@ class CoilSet:
 		zc = index_add(zc,index[:,0],np.sum(z,axis=1) / self.NS)
 		theta = np.linspace(0,2*PI,self.NS+1)[0:self.NS]
 		for m in range(1,self.NF):
-			xc = index_add(xc,index[:,m],-2.0 * np.sum(x * np.cos(m * theta), axis=1) / self.NS )
-			yc = index_add(yc,index[:,m],-2.0 * np.sum(y * np.cos(m * theta), axis=1) / self.NS )
-			zc = index_add(zc,index[:,m],-2.0 * np.sum(z * np.cos(m * theta), axis=1) / self.NS )
+			xc = index_add(xc,index[:,m], 2.0 * np.sum(x * np.cos(m * theta), axis=1) / self.NS )
+			yc = index_add(yc,index[:,m], 2.0 * np.sum(y * np.cos(m * theta), axis=1) / self.NS )
+			zc = index_add(zc,index[:,m], 2.0 * np.sum(z * np.cos(m * theta), axis=1) / self.NS )
 			xs = index_add(xs,index[:,m],-2.0 * np.sum(x * np.sin(m * theta), axis=1) / self.NS )
 			ys = index_add(ys,index[:,m],-2.0 * np.sum(y * np.sin(m * theta), axis=1) / self.NS )
 			zs = index_add(zs,index[:,m],-2.0 * np.sum(z * np.sin(m * theta), axis=1) / self.NS )
 		return np.asarray([xc,yc,zc,xs,ys,zs]) # 6 x NC x NF
 
-	def compute_frenet(self):
-		self.compute_tangent()
-		self.compute_normal()
-		self.compute_binormal()
+	def write(self, output_file):
+		""" Write coils in HDF5 output format"""
+		with tb.open_file(output_file, 'w') as f:
+			metadata = numpy.dtype([('NC', int), ('NS', int),('NF', int),('NFR',int),('ln',float),('lb',float),('NNR',int),('NBR',int),('rc',float),('NR',int)])
+			arr = numpy.array([(self.NC, self.NS, self.NF, self.NFR, self.ln, self.lb, self.NNR, self.NBR, self.rc, self.NR)],dtype=metadata)
+			f.create_table('/','metadata',metadata)
+			f.root.metadata.append(arr)
+			f.create_array('/','coilSeries',numpy.asarray(self.fc))
+			f.create_array('/','rotationSeries',numpy.asarray(self.fr))
+
+	def get_params(self):
+		return (self.fc, self.fr)
+
+	def set_params(self, params):
+		# UNPACK PARAMS
+		self.fc, self.fr = params
+		# COMPUTE COIL STUFF
+		self.compute_r_central()
+		self.compute_dsdt()
+		self.compute_length()
+		self.compute_x1y1z1()
+		self.compute_x2y2z2()
+		self.compute_x3y3z3()
+		self.compute_frenet()
+		self.compute_r() # finite build
+
+	def compute_r_central(self):
+		pass
+
+	def get_r_central(self):
+		return self.r_central
+
+	def compute_dsdt(self):
+		pass
+
+	def get_dsdt(self):
+		return self.dsdt
+
+	def calc_length(self):
+		pass
+
+	def get_length(self):
+		return self.length
 
 	def compute_x1y1z1(self):
 		pass
@@ -117,6 +111,45 @@ class CoilSet:
 	def compute_x3y3z3(self):
 		pass
 
+	def get_r1(self):
+		return self.r1
+
+	def get_r2(self):
+		return self.r2
+
+	def get_r3(self):
+		return self.r3
+
+	def compute_frenet(self):
+		self.compute_tangent()
+		self.compute_normal()
+		self.compute_binormal()
+
+	def compute_tangent(self):
+		pass
+
+	def compute_normal(self):
+		pass
+
+	def compute_binormal(self):
+		pass
+
+	def get_tangent(self):
+		return self.tangent
+
+	def get_normal(self):
+		return self.normal
+
+	def get_binormal(self):
+		return self.binormal
+
+	def compute_r(self):
+		pass
+
+	def get_r(self):
+		return self.r
+
+	"""
 	def compute_torsion(self):
 		pass
 
@@ -129,20 +162,10 @@ class CoilSet:
 	def compute_integrated_curvature(self):
 		pass
 
-	def compute_dsdz(self):
-		pass
-
 	def compute_dNdz(self):
 		pass
 
 	def compute_dBdz(self):
 		pass
+	"""
 
-	def compute_tangent(self):
-		pass
-
-	def compute_normal(self):
-		pass
-
-	def compute_binormal(self):
-		pass
