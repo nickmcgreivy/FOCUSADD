55bd6de36ba6ddaa3f4da42bffaa7462f21fd015
Nick McGreivy <mcgreivy@princeton.edu>
2020-02-17 09:47:42 -0500

    Wrote skeleton of biot-savart calculation. Not quite finished with it.

--- focusadd/lossFunctions/LossFunction.py
+++ focusadd/lossFunctions/LossFunction.py
@@ -16,5 +16,19 @@ class LossFunction:
 		We can eventually sum over this array to get the total integral over the surface. 
 
 		"""
-		pass
+		mu_0 = 1.
+		r = self.surface.get_r() # NZ+1 x NT+1 x 3
+		r = (r[1:,1:,:] + r[1:,:-1,:] + r[:-1,1:,:] + r[:-1,:-1,:]) / 4. # NZ x NT x 3
+		mu_0I = self.coil_set.get_I() * mu_0 / (self.coil_set.NNR * self.coil_set.NBR) # NC
+		r_prime = self.coil_set.get_r() # NC x NT+1 x NNR x NBR x 3
+		r_prime = (r_prime[:,1:,:,:,:] + r_prime[:,:-1,:,:,:]) / 2. # NC x NT x NNR x NBR x 3
+		mu_0Idl = mu_0I[:,np.newaxis,np.newaxis,np.newaxis,np.newaxis] * (r_prime[:,1:,:,:,:] - r_prime[:,:-1,:,:,:]) / 2. # NC x NT x NNR x NBR x 3
+		r_minus_rprime = r[np.newaxis,:,:,np.newaxis,np.newaxis,:] - r_prime[:,np.newaxis,:,:,:,:] # NC x NZ x NT x NNR x NBR x 3
+		top = np.cross(mu_0Idl[:,np.newaxis,:,:,:,:],r_minus_rprime)
+		bottom = np.linalg.norm(r_minus_rprime,axis=-1)**3
+
+		toSum = top / bottom[:,:,:,:,:,np.newaxis]
+		B = np.sum(toSum,axis=(0,3,4))
+		normal = self.surface.
+
 
