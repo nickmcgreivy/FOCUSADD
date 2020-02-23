Fixed a number of bugs in the code: computing the curvature and torsion were missing squares and cubed in the norm. Also fixed a bug in the bnsquared calculation, where the b dot n was done incorrectly. Also sped up the code significantly by computing the center of the surface points in the surface initialization rather than during the optimization (which was just a simple hack at the time).

focusadd/lossFunctions/DefaultLoss.py
@@ -12,8 +12,8 @@ class DefaultLoss(LossFunction):
 
 		Input: params, a tuple of the fourier series for the coils and a fourier series for the rotation.
 
-		Output: A scalar, which is the loss_val computed by the function. JAX will eventually differentiate 
-		this in an optimizer. 
+		Output: A scalar, which is the loss_val computed by the function. JAX will eventually differentiate
+		this in an optimizer.
 		"""
 
 		# NEED TO SET_PARAMS 
@@ -22,6 +22,8 @@ class DefaultLoss(LossFunction):
 		B_loss_val = np.sum(self.bnsquared())
 
 		len_loss_val = self.coil_set.get_total_length()
+		print(self.weight_length * len_loss_val)
+		print(B_loss_val)
 
 		return B_loss_val + self.weight_length * len_loss_val
 

focusadd/lossFunctions/LossFunction.py
@@ -19,8 +19,7 @@ class LossFunction:
 
 		"""
 		mu_0 = 1.
-		r = self.surface.get_r() # NZ+1 x NT+1 x 3
-		r = (r[1:,1:,:] + r[1:,:-1,:] + r[:-1,1:,:] + r[:-1,:-1,:]) / 4. # NZ x NT x 3
+		r = self.surface.get_r_central() # NZ x NT x 3
 		mu_0I = self.coil_set.get_I() * mu_0 / (self.coil_set.NNR * self.coil_set.NBR) # NC
 		r_prime = self.coil_set.get_r() # NC x NT+1 x NNR x NBR x 3
 		dl = (r_prime[:,1:,:,:,:] - r_prime[:,:-1,:,:,:]) / 2. # NC x NT x NNR x NBR x 3
@@ -31,9 +30,8 @@ class LossFunction:
 		bottom = np.linalg.norm(r_minus_rprime,axis=-1)**3 # NC x NZ x NT x NNR x NBR
 
 		B = np.sum(top / bottom[:,:,:,:,:,np.newaxis], axis=(0,3,4)) # NZ x NT x 3
-		nn = self.surface.get_nn() 
-		nn = (nn[1:,1:,:] + nn[1:,:-1,:] + nn[:-1,1:,:] + nn[:-1,:-1,:]) / 4.
-		return np.sum(nn * B, axis=-1)**2
+		nn = self.surface.get_nn_central() # NZ x NT x 3
+		return np.sum((nn * B)**2, axis=-1)
 
 
 

focusadd/surface/Axis.py
@@ -15,6 +15,7 @@ class Axis:
 		self.ys = ys
 		self.zc = zc
 		self.zs = zs
+		self.NF = len(self.xc)
 		self.N_zeta = N_zeta
 		self.zeta = np.linspace(0,2*PI, self.N_zeta+1)
 		self.compute_xyz()
@@ -34,7 +35,7 @@ class Axis:
 		x = np.zeros(self.N_zeta+1)
 		y = np.zeros(self.N_zeta+1)
 		z = np.zeros(self.N_zeta+1)
-		for m in range(len(self.xc)):
+		for m in range(self.NF):
 			arg = m * self.zeta
 			x += self.xc[m] * np.cos(arg) + self.xs[m] * np.sin(arg)
 			y += self.yc[m] * np.cos(arg) + self.ys[m] * np.sin(arg)
@@ -65,7 +66,7 @@ class Axis:
 		x1 = np.zeros(self.N_zeta+1)
 		y1 = np.zeros(self.N_zeta+1)
 		z1 = np.zeros(self.N_zeta+1)
-		for m in range(len(self.xc)):
+		for m in range(self.NF):
 			arg = m * self.zeta
 			x1 += -m * self.xc[m] * np.sin(arg) + m * self.xs[m] * np.cos(arg)
 			y1 += -m * self.yc[m] * np.sin(arg) + m * self.ys[m] * np.cos(arg)
@@ -81,7 +82,7 @@ class Axis:
 		x2 = np.zeros(self.N_zeta+1)
 		y2 = np.zeros(self.N_zeta+1)
 		z2 = np.zeros(self.N_zeta+1)
-		for m in range(len(self.xc)):
+		for m in range(self.NF):
 			arg = m * self.zeta
 			x2 += -m**2 * self.xc[m] * np.cos(arg) - m**2 * self.xs[m] * np.sin(arg)
 			y2 += -m**2 * self.yc[m] * np.cos(arg) - m**2 * self.ys[m] * np.sin(arg)
@@ -97,7 +98,7 @@ class Axis:
 		x3 = np.zeros(self.N_zeta+1)
 		y3 = np.zeros(self.N_zeta+1)
 		z3 = np.zeros(self.N_zeta+1)
-		for m in range(len(self.xc)):
+		for m in range(self.NF):
 			arg = m * self.zeta
 			x3 += m**3 * self.xc[m] * np.sin(arg) - m**3 * self.xs[m] * np.cos(arg)
 			y3 += m**3 * self.yc[m] * np.sin(arg) - m**3 * self.ys[m] * np.cos(arg)
@@ -117,15 +118,15 @@ class Axis:
 
 	def compute_tangent(self):
 		a0 = self.get_dsdz()
-		top = np.concatenate((self.x1[:,np.newaxis],self.y1[:,np.newaxis],self.z1[:,np.newaxis]),axis=1)
+		top = self.get_r1()
 		self.tangent = top / a0[:,np.newaxis]
 
 	def get_normal(self):
 		return self.normal
 
 	def compute_normal(self):
-		a1 = self.x2 * self.tangent[:,0] + self.y2 * self.tangent[:,1] + self.z2 * self.tangent[:,2]  
-		N = np.concatenate((self.x2[:,np.newaxis],self.y2[:,np.newaxis],self.z2[:,np.newaxis]),axis=1) - self.tangent * a1[:,np.newaxis]
+		a1 = self.x2 * self.tangent[:,0] + self.y2 * self.tangent[:,1] + self.z2 * self.tangent[:,2]
+		N = self.get_r2() - self.tangent * a1[:,np.newaxis]
 		norm = np.linalg.norm(N,axis=1)
 		self.normal = N / norm[:,np.newaxis]
 
@@ -141,7 +142,7 @@ class Axis:
 		r3 = self.get_r3()
 		cross12 = np.cross(r1, r2)
 		top = cross12[:,0] * r3[:,0] + cross12[:,1] * r3[:,1] + cross12[:,2] * r3[:,2]
-		bottom = np.linalg.norm(cross12,axis=1)
+		bottom = np.linalg.norm(cross12,axis=1)**2
 		self.torsion = top / bottom
 
 	def get_torsion(self):
@@ -152,7 +153,7 @@ class Axis:
 		r2 = self.get_r2()
 		cross12 = np.cross(r1, r2)
 		top = np.linalg.norm(cross12, axis=1)
-		bottom = np.linalg.norm(r1,axis=1)
+		bottom = np.linalg.norm(r1,axis=1)**3
 		self.curvature = top / bottom
 
 	def get_curvature(self):

focusadd/surface/Surface.py
@@ -25,16 +25,14 @@ class Surface:
 	def calc_alpha(self):
 		torsion = self.axis.get_torsion()
 		d_zeta = 2. * PI / self.NZ
-		torsionInt = np.cumsum(torsion * d_zeta)
+		torsionInt = (np.cumsum(torsion) - torsion) * d_zeta
 		zeta = self.axis.get_zeta()
 		alpha = 0.5 * self.NR * zeta + self.zeta_off
 		alpha -= torsionInt
 		self.alpha = alpha
 
-
 	def get_alpha(self):
 		return self.alpha
-
 
 	def calc_frame(self):
 		self.calc_alpha()
@@ -60,10 +58,11 @@ class Surface:
 		r += sa * np.sqrt(ep) * self.v1[:,np.newaxis,:] * ctheta[np.newaxis,:,np.newaxis]
 		r += sa * self.v2[:,np.newaxis,:] * stheta[np.newaxis,:,np.newaxis] / np.sqrt(ep)
 		self.r = r
+		self.r_central = (self.r[1:,1:,:] + self.r[1:,:-1,:] + self.r[:-1,1:,:] + self.r[:-1,:-1,:]) / 4.
+
 
 	def calc_r_coils(self,num_coils,num_segments,coil_radius):
 		r = np.zeros((num_coils,num_segments+1,3))
-		# assume s = 1
 		sa = coil_radius * self.a
 		spacing = int(self.NZ/num_coils)
 		zeta = self.axis.get_zeta()[0:self.NZ:spacing]
@@ -129,15 +128,22 @@ class Surface:
 	def get_r(self):
 		return self.r
 
+	def get_r_central(self):
+		return self.r_central
+
 	def calc_nn(self):
 		self.calc_drdt()
 		self.calc_drdz()
-		nn = np.cross(self.drdt, self.drdz)#####
+		nn = np.cross(self.drdt, self.drdz)
 		self.sg = np.linalg.norm(nn,axis=2)
 		self.nn = nn / self.sg[:,:,np.newaxis]
+		self.nn_central = (self.nn[1:,1:,:] + self.nn[1:,:-1,:] + self.nn[:-1,1:,:] + self.nn[:-1,:-1,:]) / 4.
 
 	def get_nn(self):
 		return self.nn
+
+	def get_nn_central(self):
+		return self.nn_central
 
 	def get_sg(self):
 		return self.sg