Fixed bug in bnsquared calculation, possibly there are more bugs

focusadd/coils/CoilSet.py
@@ -168,11 +168,9 @@ class CoilSet:
 		dt = 2. * PI / self.NS
 		integrand = dt * self.dsdt[:,0:-1]
 		self.length = np.sum(integrand,axis=1)
-		print(self.length)
 		dl = self.r_central[:,1:] - self.r_central[:,:-1]
 		dr = np.linalg.norm(dl,axis=-1)
 		length2 = np.sum(dr,axis=-1)
-		print(length2)
 
 
 	def get_length(self):
@@ -240,7 +238,7 @@ class CoilSet:
 		rc = self.fr[0]
 		rs = self.fr[1]
 		for m in range(self.NFR):
-			arg = self.theta * m
+			arg = self.theta * m / 2
 			carg = np.cos(arg)
 			sarg = np.sin(arg)
 			alpha += rc[:,np.newaxis,m] * carg[np.newaxis,:] + rs[:,np.newaxis,m] * sarg[np.newaxis,:]

focusadd/lossFunctions/LossFunction.py
@@ -21,14 +21,14 @@ class LossFunction:
 		mu_0 = 1.
 		r = self.surface.get_r_central() # NZ x NT x 3
 		mu_0I = self.coil_set.get_I() * mu_0 / (self.coil_set.NNR * self.coil_set.NBR) # NC
-		dl = self.coil_set.get_dl() # NC x NT x NNR x NBR x 3
-		r_prime = self.coil_set.get_r_middle()  # NC x NT x NNR x NBR x 3
-		mu_0Idl = mu_0I[:,np.newaxis,np.newaxis,np.newaxis,np.newaxis] * dl # NC x NT x NNR x NBR x 3
-		r_minus_rprime = r[np.newaxis,:,:,np.newaxis,np.newaxis,:] - r_prime[:,np.newaxis,:,:,:,:] # NC x NZ x NT x NNR x NBR x 3
-		top = np.cross(mu_0Idl[:,np.newaxis,:,:,:,:],r_minus_rprime) # NC x NZ x NT x NNR x NBR x 3
-		bottom = np.linalg.norm(r_minus_rprime,axis=-1)**3 # NC x NZ x NT x NNR x NBR
+		dl = self.coil_set.get_dl() # NC x NS x NNR x NBR x 3
+		r_prime = self.coil_set.get_r_middle()  # NC x NS x NNR x NBR x 3
+		mu_0Idl = mu_0I[:,np.newaxis,np.newaxis,np.newaxis,np.newaxis] * dl # NC x NS x NNR x NBR x 3
+		r_minus_rprime = r[np.newaxis,:,:,np.newaxis,np.newaxis,np.newaxis,:] - r_prime[:,np.newaxis,np.newaxis,:,:,:,:] # NC x NZ x NT x NS x NNR x NBR x 3
+		top = np.cross(mu_0Idl[:,np.newaxis,np.newaxis, :,:,:,:],r_minus_rprime) # NC x NZ x NT x NS x NNR x NBR x 3
+		bottom = np.linalg.norm(r_minus_rprime,axis=-1)**3 # NC x NZ x NT x NS x NNR x NBR
 
-		B = np.sum(top / bottom[:,:,:,:,:,np.newaxis], axis=(0,3,4)) # NZ x NT x 3
+		B = np.sum(top / bottom[:,:,:,:,:,:,np.newaxis], axis=(0,3,4,5)) # NZ x NT x 3
 		nn = self.surface.get_nn_central() # NZ x NT x 3
 		return np.sum((nn * B)**2, axis=-1)
 