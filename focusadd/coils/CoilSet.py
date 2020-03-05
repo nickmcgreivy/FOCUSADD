fixing shape gradients, starting to add hessian

focusadd/coils/CoilSet.py
@@ -235,12 +235,12 @@ class CoilSet:
 	def compute_frame(self):
 		alpha = np.zeros((self.NC, self.NS+1))
 		alpha += self.theta * self.NR / 2
-		torsion = self.axis.get_torsion()
-		mean_torsion = self.axis.get_mean_torsion()
+		torsion = self.get_torsion()
+		mean_torsion = self.get_mean_torsion()
 		d_theta = 2. * PI / self.NS
 		torsion = torsion - mean_torsion
 		torsionInt = (np.cumsum(torsion,axis=-1) - torsion) * d_theta
-		alpha += torsionInt
+		#alpha -= torsionInt
 		rc = self.fr[0]
 		rs = self.fr[1]
 		for m in range(self.NFR):

focusadd/shapeGradient/ShapeGradient.py
@@ -1,4 +1,5 @@
-from jax import grad
+from jax import grad, value_and_grad, hessian
+from jax.ops import index_update, index
 import jax.numpy as np
 from lossFunctions.LossFunction import LossFunction
 
@@ -11,22 +12,33 @@ class ShapeGradient:
 
 
 
-	def loss_function(self,r):
+	def loss_function(self,r,f_phys=True,f_eng=True):
 		dl = (r[:,1:,:,:,:] - r[:,:-1,:,:,:])
 		r_middle = (r[:,1:,:,:,:] + r[:,:-1,:,:,:]) / 2.
 		B_loss_val = np.sum(LossFunction.bnsquared(self.surface.get_r_central(),\
 			self.coil_set.get_I(),dl,r_middle,\
 			self.surface.get_nn(), self.surface.get_sg(), self.surface.NT, self.surface.NZ,\
 			 self.coil_set.NNR, self.coil_set.NBR))
-		len_loss_val = np.sum(np.linalg.norm(dl,axis=-1))
-		return B_loss_val + self.weight_length * len_loss_val
+		len_loss_val = self.coil_set.get_total_length()
+		loss = 0.
+		if f_phys:
+			loss += B_loss_val
+		if f_eng:
+			loss += self.weight_length * len_loss_val
+		return loss
 
 
+	def coil_gradient(self,f_phys=True,f_eng=True):
+		grad_loss = value_and_grad(self.loss_function)
+		loss, shape_grad = grad_loss(self.coil_set.get_r(),f_phys=f_phys,f_eng=f_eng)
+		shape_grad = index_update(shape_grad, index[:,0,:,:,:], (shape_grad[:,0,:,:,:] + shape_grad[:,-1,:,:,:]))[:,:-1,:,:,:]
+		return loss, shape_grad
 
-
-	def coil_gradient(self):
-		grad_loss = grad(self.loss_function)
-		return grad_loss(self.coil_set.get_r())
+	def coil_hessian(self,f_phys=True, f_eng=True):
+		hessian_func = hessian(self.loss_function)
+		print(self.coil_set.get_r().shape)
+		H = hessian_func(self.coil_set.get_r(),f_phys=f_phys,f_eng=f_eng)
+		print(H.shape)
 
 
 