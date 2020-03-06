Fixed factor of 2 in coilset finite-build modes.

focusadd/coils/CoilSet.py
@@ -168,10 +168,6 @@ class CoilSet:
 		dt = 2. * PI / self.NS
 		integrand = dt * self.dsdt[:,0:-1]
 		self.length = np.sum(integrand,axis=1)
-		dl = self.r_central[:,1:] - self.r_central[:,:-1]
-		dr = np.linalg.norm(dl,axis=-1)
-		length2 = np.sum(dr,axis=-1)
-
 
 	def get_length(self):
 		return self.length
@@ -238,13 +234,13 @@ class CoilSet:
 		torsion = self.get_torsion()
 		mean_torsion = self.get_mean_torsion()
 		d_theta = 2. * PI / self.NS
-		torsion = torsion - mean_torsion
+		torsion = torsion - mean_torsion[:,np.newaxis]
 		torsionInt = (np.cumsum(torsion,axis=-1) - torsion) * d_theta
 		#alpha -= torsionInt
 		rc = self.fr[0]
 		rs = self.fr[1]
 		for m in range(self.NFR):
-			arg = self.theta * m / 2
+			arg = self.theta * m
 			carg = np.cos(arg)
 			sarg = np.sin(arg)
 			alpha += rc[:,np.newaxis,m] * carg[np.newaxis,:] + rs[:,np.newaxis,m] * sarg[np.newaxis,:]
@@ -280,7 +276,7 @@ class CoilSet:
 		return self.torsion
 
 	def compute_mean_torsion(self):
-		self.mean_torsion = np.mean(self.torsion[:,:-1])
+		self.mean_torsion = np.mean(self.torsion[:,:-1],axis=-1)
 
 	def get_mean_torsion(self):
 		return self.mean_torsion