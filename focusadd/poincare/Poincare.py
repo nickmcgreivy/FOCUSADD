de96bbc11718e85e25617e7c46c4ebb3b314d546
Nick McGreivy <mcgreivy@princeton.edu>
2020-04-01 11:32:25 -1000

    Validated Poincare.py

--- focusadd/poincare/Poincare.py
+++ focusadd/poincare/Poincare.py
@@ -17,29 +17,33 @@ class Poincare():
 		"""
 			Inputs:
 
-			r, theta, z: The coordinates of the point we want the 
-			magnetic field at
+			r, theta, z : The coordinates of the point we want the magnetic field at. Cylindrical coordinates.
 
 			Outputs: 
 
-			B(r): The magnetic field at r, created by the currents in the coils. This is a length-3 vector. 
+			B_z, B_theta, B_z : the magnetic field components at the input coordinates created by the currents in the coils. Cylindrical coordinates.
 		"""
 		x = r * np.cos(theta)
 		y = r * np.sin(theta)
-		r = np.asarray([x,y,z])
+		xyz = np.asarray([x,y,z])
 		l = self.coil_set.get_r_middle()
 		I = self.coil_set.get_I()
 		NNR = self.coil_set.NNR
 		NBR = self.coil_set.NBR
 		dl = self.coil_set.get_dl()
 		mu_0 = 1. 
-		mu_0I = I * mu_0 / (NNR * NBR) # NC
+		mu_0I = I * mu_0 # NC
 		mu_0Idl = mu_0I[:,np.newaxis,np.newaxis,np.newaxis,np.newaxis] * dl # NC x NS x NNR x NBR x 3
-		r_minus_l = r[np.newaxis,np.newaxis,np.newaxis,np.newaxis,:] - l[:,:,:,:,:] # NC x NS x NNR x NBR x 3
+		r_minus_l = xyz[np.newaxis,np.newaxis,np.newaxis,np.newaxis,:] - l[:,:,:,:,:] # NC x NS x NNR x NBR x 3
 		top = np.cross(mu_0Idl,r_minus_l) # NC x x NS x NNR x NBR x 3
 		bottom = np.linalg.norm(r_minus_l,axis=-1)**3 # NC x NS x NNR x NBR
-		B = np.sum(top / bottom[:,:,:,:,np.newaxis], axis=(0,1,2,3)) # 3
-		return B
+		B_xyz = np.sum(top / bottom[:,:,:,:,np.newaxis], axis=(0,1,2,3)) # 3, xyz coordinates
+		B_x = B_xyz[0]
+		B_y = B_xyz[1]
+		B_z = B_xyz[2]
+		B_r = B_x * np.cos(theta) + B_y * np.sin(theta)
+		B_theta = - B_x * np.sin(theta) + B_y * np.cos(theta)
+		return B_r, B_theta, B_z
 
 	def odeStep(self,r,theta,z):
 		"""
@@ -53,29 +57,38 @@ class Poincare():
 
 			Outputs:
 
-			dr, dtheta, zd: The coordinates of the magnetic field line after a single integration
-			step of size dtheta. 
+			Fr : dr/dtheta, the change in the r-position of a field line per change in theta
+			Fz : dz/dtheta, the change in the z-position of a field line per change in theta
 		"""
-		B = self.computeB(r, theta, z) 
-		B_x = B[0]
-		B_y = B[1]
-		B_z = B[2]
-		B_r = B_x * np.cos(theta) + B_y * np.sin(theta)
-		B_theta = - B_x * np.sin(theta) + B_y * np.cos(theta)
+		B_r, B_theta, B_z = self.computeB(r, theta, z) 
 		Fr = (B_r * r / B_theta)
 		Fz = (B_z * r / B_theta)
 		return Fr, Fz
 
 
 	def rkStep(self, r_i, theta_i, z_i,dtheta):
-		""" Range-Kutta fourth order step """ 
-		Fr_1, Fz_1 = self.odeStep(r_i,theta_i,z_i,)
-		r_2 = r_i + dtheta/2. * Fr_1
-		z_2 = z_i + dtheta/2. * Fz_1
+		""" 
+		A single Runge-Kutta fourth order step 
+
+		Inputs
+
+		r_i , theta_i, z_i : The initial coordinates of the magnetic field line
+		dtheta : the step size of the Runge-Kutta step
+
+		Returns
+
+		r_f, theta_f, z_f : The final coordinates of the magnetic field line after
+		the Runge-Kutta step
+
+
+		""" 
+		Fr_1, Fz_1 = self.odeStep(r_i,theta_i,z_i)
+		r_2 = r_i + (dtheta/2.) * Fr_1
+		z_2 = z_i + (dtheta/2.) * Fz_1
 
 		Fr_2, Fz_2 = self.odeStep(r_2,theta_i + dtheta/2.,z_2)
-		r_3 = r_i + dtheta/2. * Fr_2
-		z_3 = z_i + dtheta/2. * Fz_2
+		r_3 = r_i + (dtheta/2.) * Fr_2
+		z_3 = z_i + (dtheta/2.) * Fz_2
 
 		Fr_3, Fz_3 = self.odeStep(r_3, theta_i + dtheta/2., z_3)
 		r_4 = r_i + dtheta * Fr_3
@@ -121,16 +134,14 @@ class Poincare():
 		thetas.append(theta_i)
 		zs.append(z_i)
 		dtheta = 2. * PI / N_step
-		theta = theta_i
+
 		r = r_i
 		theta = theta_i
 		z = z_i
+
 		for i in range(N_poincare):
 			for j in range(N_step):
 				r, theta, z = self.rkStep(r,theta,z,dtheta)
-				#rs[i*N_step + j+1] = r
-				#thetas[i*N_step + j+1] = theta
-				#zs[i*N_step+ j+1] = z
 			theta = theta % (2 * PI)
 			rs.append(r)
 			thetas.append(theta)
@@ -145,23 +156,44 @@ class Poincare():
 
 	def getPoincarePoints(self,N_step,N_poincare,theta,radii):
 		"""
-			Loop over N_lines points in the plot, equally spaced in toroidal
+
+			NOTE: THIS ONLY WORKS FOR THETA = 0 RIGHT NOW
+
+			Loop over N_poincare points in the plot, equally spaced in toroidal
 			radius starting from the magnetic axis at a given theta, and 
 			creating a list of points where the plot punctures.
+
+			Inputs:
+
+			N_step : number of RK steps per poincare transit, dtheta = 2pi/N_step
+			N_poincare : number of times we puncture the poincare plot, starting at radius r
+			theta : Which theta will our poincare plot track? We are getting a poloidal cross-section at theta.
+			radii : list of radii r which we start at, away from our magnetic axis. We expect this to range from 0 
+			to slightly higher than 1. 
+
+			Outputs:
+
+			rs, thetas, zs : lists of floating point numbers where the field lines intersect at a given theta. 
+
 		"""
 
 		x_axis,y_axis,z_axis = self.axis.get_r_from_zeta(theta)
-		axisNormalAtZero = self.axis.get_normal()[0,:]
+		r_axis = np.sqrt(x_axis ** 2 + y_axis ** 2)
+		v1, _ = self.axis.get_frame() # v1, v2
+		v1 = v1[0,:] # at zeta = 0 THIS LINE BREAKS THE CODE
+		ep = self.axis.epsilon
+		saep = self.axis.a * self.surface.s * np.sqrt(ep)
 		ct = np.cos(theta)
 		st = np.sin(theta)
 		rs = []
 		thetas = []
 		zs = []
 		for r in radii:
-			x = x_axis + r * ct
-			y = y_axis + r * st
-			z = z_axis
-			rlist, thetalist, zlist = self.odeIntegrate(r + (np.sqrt(x**2 + y**2)),theta,z,N_step, N_poincare)
+			v1_normalized = r * saep * v1
+			r_i = r_axis + ct * v1_normalized[0] + st * v1_normalized[1]
+			# theta = theta
+			z_i = z_axis + v1[2]
+			rlist, thetalist, zlist = self.odeIntegrate(r_i, theta, z_i, N_step, N_poincare)
 			rs = rs + rlist
 			thetas = thetas + thetalist
 			zs = zs + zlist
