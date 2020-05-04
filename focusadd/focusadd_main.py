ba449115412efc1f2ca39c6c68d4d468d9325c40
Nick McGreivy <mcgreivy@princeton.edu>
2020-05-04 08:39:05 -1000

    Changed number of iterations

--- focusadd/focusadd_main.py
+++ focusadd/focusadd_main.py
@@ -20,7 +20,7 @@ import csv
 PI = math.pi
 def setArgs():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-n","--numIter", help="Number of iterations by the optimizer", default=500)
+	parser.add_argument("-n","--numIter", help="Number of iterations by the optimizer", default=10)
 	parser.add_argument("-nt","--numTheta", help="Number of gridpoints in theta (poloidal angle) on the magnetic surface", default=32)
 	parser.add_argument("-nz","--numZeta", help="Number of gridpoints in zeta (toroidal angle) on the magnetic surface", default=64)
 	parser.add_argument("-nc","--numCoils", help="Number of coils", default=8)
